1bbdab44d3d4dbcc807223804b62e0b3d4c78a1c
218503 <218503@student.pwr.edu.pl>
2016-04-18 13:14:39 +0200

    praca na laboratorium, nie trafiona implementacja

--- main.cpp
+++ main.cpp
@@ -1,54 +1,62 @@
 #include <stdio.h>
-#include "tablica.hh"
+#include <iostream>
+#include <math.h>
+using namespace std;
 
-
-class komorka{
+class element{
 public:
-  string slowo_klucz;
   int wartosc;
+  int *lewy, *prawy;
+
 };
 
-class it_a{
-public: 
-  virtual komorka odczyt(string)=0;
-  virtual void zapis(komorka)=0;
-  virtual int f_hasz(string)=0;
- 
-};
+class drzewo{
+public:
+  int korzen;
+  int **drz;
+  int i,j,ile;
 
-
-class t_a : public it_a{
-private:
-  tab<tab<komorka>> *tab_indeksow;
-public: 
-  virtual komorka odczyt(string);
-  virtual void zapis(komorka);
-  virtual int f_hasz(string slowo){
-    switch(slowo[0]){
-      if((slowo<=90)&&(slowo>=65)){
-	return slowo[0]-65;}
+  void wypelnij(int ilosc){
+    int w,k,zm=1;
+    ile=ilosc;
+    w=sqrt(ilosc)+1;
+    cout<<"w"<<w<<"  ilosc "<<ilosc<<endl;
+    drz=new int *[w];
+    drz[0]=new int[1];
+    drz[0][0]=ilosc/2;
+    drz[1]=new int[2];
+    drz[1][0]=drz[0][0]-2;
+    drz[1][1]=drz[0][0]+2;
+    for(i=2;i<w;i++){
+      k=pow(2,i);
+      drz[i]=new int[k];
+      for(j=0;j<k;j++){
+	drz[i][j+j]=drz[i-1][j]-2;
+	drz[i][j+j+1]=drz[i-1][j]+2;
+      }      
     }
-
   }
+  
+
+  void wypisz()
+  {int i,j,w,p;
+    w=sqrt(ile)+1;
+    for(i=0;i<w;i++){
+      p=pow(2,i);
+      for(j=0;j<p;j++){
+	cout<<drz[i][j]<<" ";
+      }
+      cout<<endl;
+    }
+     
+  }
+
 };
-
-t_a::f_hasz(){
-
-  return 
-}
 
 int main(){
-  int n=10; //liczba slotow
   
-  
+  drzewo d;
+  d.wypelnij(10);
+  d.wypisz();
   return 0;
 }
-
-
-komorka operator[](tab<tab<komorka>> t_i,string kl){
-
-  
-}  
-
-
-
