1a2ed0dcab349013965b0ef1e9c207c4066a4cf3
218503 <218503@student.pwr.edu.pl>
2016-05-09 10:21:17 +0200

    Add files via upload

---
+++ graf/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "graf.hh"
+#include "lista.hh"
+#include "tablica.hh"
+
+
+int main(){
+
+  test_grafu g;
+  g.prepare(11);
+  g.run();
+
+  return 0;
+}
---
+++ graf/src/tablica.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <iostream>
+#include "tablica.hh"
+
+using namespace std;
+
