587aeb9afc20fa8a825939d07e0e3a6faa84447a
218503 <218503@student.pwr.edu.pl>
2016-05-16 03:35:46 +0200

    Pomiar czasu DFS

--- graf/src/main.cpp
+++ graf/src/main.cpp
@@ -3,13 +3,28 @@
 #include "graf.hh"
 #include "lista.hh"
 #include "tablica.hh"
+#include <sys/time.h>
+#define RG 1000
 
 
 int main(){
 
-  test_grafu g;
-  g.prepare(11);
+  test_grafu g(RG);
+  struct timeval start;
+  struct timeval end;
+  long long int ms=0, s=0,us=0;
+  
+  gettimeofday(&start,NULL);
+  g.prepare();
   g.run();
+  gettimeofday(&end,NULL); 
+  s=(end.tv_sec-start.tv_sec);
+  ms=(end.tv_usec-start.tv_usec)/1000;
+  us=(end.tv_usec-start.tv_usec)%1000;
+  cout<<"|   Czas: "<<s<<"s "<<ms<<"ms "<<us<<"us"<<endl;
+  cout<<"|   Liczba_wierzcholkow: "<<RG<<endl;
+
+  
 
   return 0;
 }
